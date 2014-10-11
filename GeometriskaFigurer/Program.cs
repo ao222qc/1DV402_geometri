02478f2b0f5bc5dd2bfe16d9725bd708a9aa91c3
ao222qc <ao222qc@student.lnu.se>
2014-10-11 18:58:11 +0200

    Done some stuff

---
+++ GeometriskaFigurer/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GeometriskaFigurer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                {
+                    ViewMenu();
+                    int input = int.Parse(Console.ReadLine());
+                    if (input == 0)
+                    {
+                        return;
+                    }
+                    if (input == 1)
+                    {
+                        ViewShapeDetail(CreateShape(ShapeType.Ellipse));
+                        break;
+                    }
+                    if (input == 2)
+                    {
+                        ViewShapeDetail(CreateShape(ShapeType.Rectangle));
+                        break;
+                    }
+                    else
+                    {
+                        Console.BackgroundColor = ConsoleColor.DarkRed;
+                        Console.WriteLine("\nPlease enter a menu choice between 0-2.\n");
+                        Console.ResetColor();
+                    }
+
+                }   
+            }
+        }
+   
+        private static Shape CreateShape(ShapeType shapeType)
+        {
+
+            double length = ReadDoubleGreaterThanZero("Input length.\n");
+            double width = ReadDoubleGreaterThanZero("Input width.\n");
+           
+            if (shapeType == ShapeType.Ellipse)
+            {
+                Console.WriteLine("\nELLIPSE!\n");
+                Ellipse ellipse = new Ellipse(length, width);
+                return ellipse;
+            }
+            if (shapeType == ShapeType.Rectangle)
+            {
+                Console.WriteLine("RECTANGLE!\n");
+                Rectangle rectangle = new Rectangle(length, width);
+                return rectangle;  
+            }
+            return null;
+        }
+
+        private static double ReadDoubleGreaterThanZero(string prompt) 
+        {
+
+            while (true)
+            {               
+                    Console.Write(prompt);
+
+                    double input = double.Parse(Console.ReadLine());
+
+                    if (input > 0)
+                    {
+                        return input;
+                    }         
+                    else 
+                    {
+                     Console.BackgroundColor = ConsoleColor.DarkRed; 
+                     Console.WriteLine("Error. Please enter a valid sum.\n");
+                     Console.ResetColor();
+
+                    }
+            }
+
+            //return input;
+        }
+        private static void ViewMenu()
+        {
+            Console.WriteLine("\nGeometrical figures.\n");
+            Console.WriteLine("0: Cancel.\n");
+            Console.WriteLine("1: Ellipse.\n");
+            Console.WriteLine("2: Rectangle.\n");
+            Console.WriteLine("\n=====================================\n");
+            Console.Write("\nInput menu choice [0-2]: ");
+        }
+        private static void ViewShapeDetail(Shape shape)
+        {
+            Console.WriteLine("\n{0}\n", shape.ToString());
+            Console.WriteLine("=====================================\n");
+        }
+
+
+    }
+}
---
+++ GeometriskaFigurer/Shape.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GeometriskaFigurer
+{
+    abstract class Shape
+    {
+       private double _length;
+       private double _width;
+
+       public abstract double Area
+       {
+           get;
+       }
+        public double Length
+        {
+            get { return _length; }
+            set 
+            {
+                if (value <= 0)
+                { throw new ArgumentException();}
+
+                _length = value;
+            }
+        }
+        public abstract double Perimeter
+        {
+            get;
+
+        }
+        public double Width
+        {
+            get { return _width; }
+
+            set  
+            {
+                if (value <= 0)
+                { throw new ArgumentException(); }
+                _width = value;
+            }
+        }
+        protected Shape(double length, double width)
+        {
+            Length = length;
+            Width = width;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder hej = new StringBuilder();
+            hej.AppendFormat("Length    : {0,5}\nWidth     : {1,5}\nPerimeter : {2,5:F1}\nArea      : {3,5:F1}",
+                 Length, Width, Perimeter, Area);
+            return hej.ToString();
+        }
+
+
+
+
+    }
+    public enum ShapeType {Ellipse, Rectangle};
+}
